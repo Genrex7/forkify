116fb1424c350a6e2817c51cfb734e85b137d03d
Deepak Kumar Meena <108786618+Genrex7@users.noreply.github.com>
2023-08-15 00:39:04 +0530

    Update config.js
    Add your own API key.

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = 'a10c462e-c8ab-4482-880e-9b861c63f639';
+export const KEY = 'YOUR_API_KEY';
 export const MODAL_CLOSE_SEC = 2.5;
